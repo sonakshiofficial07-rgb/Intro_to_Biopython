be1528f33b5fafa45362adeff00a519482aacbf1
sonakshiofficial07-rgb <sonakshi.official07@gmail.com>
2026-05-31 20:36:23 +0530

    Implement DNA counting functionality
    Add a script to count DNA nucleotide occurrences.

---
+++ dna_counting.py
@@ -0,0 +1,11 @@
+dna = input("ENTER THE DNA SEQUENCE:").upper()
+
+a= dna.count("A")
+t= dna.count("T")
+c= dna.count("C")
+g= dna.count("G")
+
+print("A =", a)
+print("T =", t)
+print("C =", c)
+print("G =", g)
